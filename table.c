cf76904a5b37f091ba7b0645dc00c7607e44c6e9
Yuriy Kulinchenko <ykulinchenko@gmail.com>
2025-08-03 15:04:22 +0100

    Added tableGet, tableAddAll operations, some code implemented for tombstones

--- table.c
+++ table.c
@@ -18,26 +18,52 @@ void freeTable(Table* table) {
     initTable(table);
 }
 
-void growCapacity(Table* table) {
-    int oldCapacity = table->capacity;
-    table->capacity = oldCapacity < 8 ? 8 :oldCapacity * 2;
-    table->entries = GROW_ARRAY(Entry, table->entries, oldCapacity, table->capacity);
-}
-
 Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    // Returns entry pointer with key "key"
+    // If this is not present, returns first empty slot
+    // If any tombstones are encountered on the way, the tombstone is returned
+    Entry* tombstone = NULL;
     int index = (int) key->hash % capacity;
     for (;;) {
         Entry* entry = &entries[index];
-        if (entry->key == key || entry->key == NULL) {
+        bool isNull = entry->key == NULL && !AS_BOOL(entry->value); // Is not null if tombstone
+        if (isNull || entry->key == key) { // BAD - uses reference equality
             return entry;
         }
         index = (index + 1) % capacity;
     }
 }
 
+void adjustCapacity(Table* table, int capacity) {
+    Entry* entries = ALLOCATE(Entry, capacity);
+    for (int i = 0; i < capacity; i++) {
+        entries->key = NULL;
+        entries->value = BOOL_VAL(false);
+    }
+
+    // Now, hashes must be recomputed, entries put in etc
+
+    int count = 0;
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+
+        Entry* position = findEntry(entries, capacity, entry->key);
+        position->key = entry->key;
+        position->value = entry->value;
+        count++;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+    table->count = count;
+}
+
 bool tableSet(Table* table, ObjString* key, Value value) {
-    if (table->count + 1 > TABLE_MAX_LOAD * table->capacity) {
-        growCapacity(table);
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = table->capacity < 8 ? 8 : table->capacity * 2;
+        adjustCapacity(table, capacity);
     }
     // Guaranteed to have space now:
     Entry* entry = findEntry(table->entries, table->capacity, key);
@@ -47,4 +73,23 @@ bool tableSet(Table* table, ObjString* key, Value value) {
     entry->key = key;
     entry->value = value;
     return isNewKey;
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    // If the value exists, it is stored in the given pointer location
+    // Returns true if value is present, false otherwise
+    if (table->count == 0) return false;
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key != NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
+void tableAddAll(Table* from, Table* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* fromEntry = &from->entries[i];
+        if (fromEntry->key != NULL) tableSet(to, fromEntry->key, fromEntry->value);
+    }
 }
--- table.h
+++ table.h
@@ -22,6 +22,8 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
 bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
 
 #endif
