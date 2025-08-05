66202a17480f8200425bea3913987500dd94e15d
Yuriy Kulinchenko <ykulinchenko@gmail.com>
2025-08-05 16:24:58 +0100

    Overhauled value printing code to use new 'valueToString' function, which returns a valid string instead of directly printing it

--- value.c
+++ value.c
@@ -1,6 +1,7 @@
 #include "value.h"
 #include "memory.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "object.h"
 
@@ -25,30 +26,45 @@ void freeValueArray(ValueArray* array) {
     reallocate(array->values, array->capacity, 0);
 }
 
-void printValue(Value value) {
+char* objToString(Obj* object) {
+    char* chars;
+    switch (object->type) {
+        case OBJ_STRING: {
+            asprintf(&chars, "\"%s\"",((ObjString*) object)->chars);
+            break;
+        }
+        default: asprintf(&chars, "unrecognized object"); break;
+    }
+    return chars;
+}
+
+char* valueToString(Value value) {
+    char* chars;
     switch (value.type) {
         case VAL_NUMBER: {
-            printf("%g", AS_NUMBER(value));
+            asprintf(&chars,"%g", AS_NUMBER(value));
             break;
         }
         case VAL_NIL: {
-            printf("nil");
+            asprintf(&chars, "nil");
             break;
         }
         case VAL_BOOL: {
-            printf(AS_BOOL(value) ? "true" : "false");
+            asprintf(&chars, AS_BOOL(value) ? "true" : "false");
             break;
         }
 
         case VAL_OBJ: {
-            Obj* objPtr = AS_OBJ(value);
-            switch (objPtr->type) {
-                case OBJ_STRING: {
-                    printf("\"%s\"",((ObjString*) objPtr)->chars);
-                    break;
-                }
-                default: printf("unrecognized object"); break;
-            }
+            return objToString(AS_OBJ(value));
         }
+
+        default: asprintf(&chars, "unrecognized value"); break;
     }
+    return chars;
+}
+
+void printValue(Value value) {
+    char* chars = valueToString(value);
+    printf(chars);
+    free(chars);
 }
